3e2a0d399a3106096b2209ebd014704c1b2acc76
dankastv16 <44006418+dankastv16@users.noreply.github.com>
2020-08-14 16:01:24 +0300

    Create wakeup.py

---
+++ wakeup.py
@@ -0,0 +1,9 @@
+  
+"""
+This script is used with Heroku scheduler.
+It intends to call a wakeup function in the main.py (if necessary, restart the main web dyno)
+"""
+import requests
+
+if __name__ == '__main__':
+    requests.get("https://saltybotessa.herokuapp.com/wakeup/")
